a4c069c4c59e349d1c7e281b4da3fa3e3370590e
Flavio Fraschetti <flavio@riseup.ai>
2020-03-26 20:11:15 +0000

    create and style login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { FiLogIn } from "react-icons/fi";
 
 import "./styles.css";
 
+import heroesImg from "../../assets/heroes.png";
+import logoImg from "../../assets/logo.svg";
+
 export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be the Hero" />
+
+        <form>
+          <h1>Login</h1>
+          <input placeholder="Your ID" />
+          <button className="button" type="submit">
+            Login
+          </button>
+          <a href="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Register
+          </a>
+        </form>
+      </section>
+      <img src={heroesImg} alt="Heroes" />
     </div>
   );
 }
