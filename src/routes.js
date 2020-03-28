9096c3525c2e7eb9c3a589ba6776cdf22098ed1b
Flavio Fraschetti <flavio@riseup.ai>
2020-03-28 08:23:24 +0000

    update routes

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,17 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Login from "./pages/Login/";
 import Register from "./pages/Register/";
+import Profile from "./pages/Profile/";
+import NewIncident from "./pages/NewIncident";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
-        <Route path="/" component={Register} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/incident/new" component={NewIncident} />
       </Switch>
     </BrowserRouter>
   );
